52db3f03400c6f213876e0daba725eb91469d683
williamblood <w.blood@icloud.com>
2020-01-15 15:33:54 -0800

    Added option to call function checking for a perfect pangram

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-function isPangram(paramString) {
+/**
+ * Returns true if input string is a pangram.
+ * Returns true if called with {string} | {boolean}
+ * and if input string is a perfect pangram.
+ * 
+ * @param {string} paramString - assumes string is a real sentence, special characters OK.
+ * @param {boolean} perfectPangramOpt - additional restrictions: unique, no special characters, whitespace OK.
+ * @return {boolean} true | false
+ */
+
+function isPangram(paramString, perfectPangramOpt) {
 	const alphabet = "abcdefghijklmnopqrstuvwxyz";
 	const alphaRray = [];
 	const stringArray = [];
-	// Convert string to lowercase char array like alphabet
+
+	// Convert strings to lowercase char array without spaces
 	let string = paramString.replace(/\s+/g, "").toLowerCase();
 
 	for (let letter of alphabet) {
@@ -13,7 +24,11 @@ function isPangram(paramString) {
 	}
 	console.log(stringArray);
 	console.log(alphaRray);
+
+	// Compare each letter in string array to alphabet array
+
 	for (let i = 0; i < stringArray.length; i++) {
+		let unique = false;
 		for (let j = 0; j < alphaRray.length; j++) {
 			if (stringArray[i] === alphaRray[j]) {
 				console.log(
@@ -21,14 +36,23 @@ function isPangram(paramString) {
 				);
 				console.log(`Removing ${alphaRray[j]}`);
 				alphaRray.splice(j, 1);
+				unique = true;
 				console.log("New array: " + alphaRray);
 				break;
 			}
 		}
+		// Checks for a perfect pangram if user activates option
+		if (!unique && perfectPangramOpt) {
+			console.error(`${unique} - Not a perfect pangram.`);
+			return false;
+		}
 	}
+	// If any letters remain in alphabet, string is not a pangram
 	if (alphaRray.length) {
-		console.warn("Not a Pangram.");
+		console.warn("Not a pangram.");
 		return false;
 	}
+	// If alphabet is empty, input is a pangram
+	console.log("This is a pangram.");
 	return true;
 }
