9b27a822f67f5cc1e0ecfd57eaa506fb13da7fc0
williamblood <w.blood@icloud.com>
2020-01-16 01:11:05 -0800

    ...

--- higher-order/scratch.js
+++ higher-order/scratch.js
@@ -1,5 +1,18 @@
+/**
+ * Returns an anonymous function that returns true
+ * if a number is within the range of x and y 
+ * when called.
+ * @param {*} x - set minimum barrier value.
+ * @param {*} y - set maximum barrier value.
+ */
+
 function between(x, y) {
 	return function(num) {
 		return num >= x && num <= y;
 	};
 }
+
+// Examples:
+const isAdult = between(18, 99);
+const isMinor = between(0, 18);
+const isNiceWeather = between(65, 79);
