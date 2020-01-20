3da418c9e89af3916ef8590495d61bffebfa7ed9
William Blood <william.blood@icloud.com>
2020-01-19 22:07:49 -0800

    rest and spread algorithms

--- spread-operator/spread.js
+++ spread-operator/spread.js
@@ -1,3 +1,4 @@
+/* Legacy way */
 let nums = [ 4, 10, 2, 4, 9, 3, 5, 1, 0 ];
 console.log(nums);
 nums = {
@@ -5,8 +6,27 @@ nums = {
 };
 console.log(nums);
 
-function sum(x, y, z) {
+const sumExample = function(x, y, z) {
 	console.log(arguments[0]);
 	return x + y + z;
-}
+};
 console.log(sum(4, 20, 0));
+
+function sum() {
+	const argumentsToArray = [ ...arguments ];
+	return argumentsToArray.reduce((container, element) => {
+		return container + element;
+	});
+}
+
+/* New Standard way */
+
+// Rest - spread operator for infinite params/args
+function sumRest(...numbers) {
+	return numbers.reduce((container, element) => {
+		return container + element;
+	});
+}
+// Rest
+const mult = (...numbers) =>
+	numbers.reduce((total, val) => total * val);
