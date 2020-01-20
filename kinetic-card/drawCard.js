6f6d59528f5aec9b1a8e187c8bca4d817ae52338
williamblood <w.blood@icloud.com>
2020-01-20 13:23:06 -0800

    more algs: Computed properties

--- kinetic-card/drawCard.js
+++ kinetic-card/drawCard.js
@@ -25,18 +25,18 @@ const card = drawCard();
 console.log(card);
 
 // Replace value string with this array for multi-char strings
-// const value = [
-//     "A",
-//     "2",
-//     "3",
-//     "4",
-//     "5",
-//     "6",
-//     "7",
-//     "8",
-//     "9",
-//     "10",
-//     "J",
-//     "Q",
-//     "K"
-// ];
+const value2 = [
+	"A",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"10",
+	"J",
+	"Q",
+	"K"
+];
---
+++ objects/computed-properties/app.js
@@ -0,0 +1,33 @@
+const role = "President";
+const person = "Maximillion Pegasus";
+const role2 = "Contestant";
+const person2 = "Yugi Moto";
+
+/* (New) dynamic keys */
+const players = {
+	[role]        : person,
+	[role2]       : person2,
+	[(1 + 1) / 2]: "one"
+};
+
+/**
+ * Adds a new property to an object and returns the copy.
+ * @param {object} object - Object to add properties.
+ * @param {*} key - Name of property. 
+ * @param {*} value - Contents.
+ */
+const addProperty = (object, key, value) => {
+	return (object = {
+		...object,
+		[key] : value
+	});
+};
+
+const updatedPlayers = addProperty(players, "Me", "William Blood");
+// expected output : {1: "one", President: "Maximillion Pegasus", Contestant: "Yugi Moto", Me: "William Blood"}
+
+/* (Legacy) Dynamic Keys*/
+// initialize obj
+const players2 = {};
+players2[role] = person;
+players2[role2] = person2;
