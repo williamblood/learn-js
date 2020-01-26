716e29dd8be6eb20d8294dfe9bddbc0c702223ae
William Blood <william.blood@icloud.com>
2020-01-26 13:34:44 -0800

    how to use setTimeout & more call stack stuff

--- callbacks/set-timeout.js
+++ callbacks/set-timeout.js
@@ -1,12 +1,19 @@
+/* Open dev tools to check the call stack and play with breakpoints */
 const btn = document.querySelector("button");
 
+// Note:
+//  setTimeout contents are executed last.
+//  the function is placed on top as the last in the call stack,
+//  allowing any button changes to occur at the end of the delay.
+
 const moveAfter = (element, pixels, delay) => {
-	element.innerText = `Moving in ${delay / 1000} seconds`;
+	element.innerText = `Moving in ${delay / 1000} seconds..`;
 	setTimeout(() => {
 		element.style.transform = `translateX(${pixels}px)`;
 		element.innerText = "Move";
 	}, delay);
 };
+
 btn.addEventListener("click", () => {
 	moveAfter(btn, 200, 2000);
 });
