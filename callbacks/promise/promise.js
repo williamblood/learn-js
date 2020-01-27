c33008d61b439d94c98eb780c2f0fae1f3ea4a8a
William Blood <william.blood@icloud.com>
2020-01-26 23:32:51 -0800

    Fake HTTP request with promises: loads 'contact' page

--- callbacks/promise/promise.js
+++ callbacks/promise/promise.js
@@ -1,18 +1,30 @@
+const contactURL = "/contact";
+
 const get = url => {
 	return new Promise((resolve, reject) => {
-		if (url === "amskdjaksdjalksjd112ielsa") resolve();
-		else reject();
+		const pages = {
+			"/users"   : [
+				{ id: 9, name: "Billy" },
+				{ id: 32, name: "Sophie" }
+			],
+			"/contact" : "Pretend this is the contact page!"
+		};
+		const requestedData = pages[url];
+		if (pages[url]) resolve({ status: 200, requestedData });
+		else reject({ status: 404 });
 	});
 };
 
-const url = "amskdjaksdjalksjd112ielsa";
-get(url)
-	.then(() => {
-		console.log(`Key: ${url}`);
+get(contactURL)
+	.then(response => {
+		console.log(`Status code: ${response.status} OK`);
 		console.log("Resolved. Content accessed.");
+		console.log(
+			`Loading page and revealing contents: ${response.requestedData}`
+		);
 	})
-	.catch(() => {
-		console.warn("Rejected");
+	.catch(response => {
+		console.warn(`There was an error: ${response.status}`);
 	});
 
-// .then runs only if promise is resolved
+// then() runs only if promise is resolved
