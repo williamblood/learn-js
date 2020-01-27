5ea7096bc4535904dd56838d51d39d4043cd4fad
William Blood <william.blood@icloud.com>
2020-01-27 00:07:50 -0800

    mock-up HTTP request, returns requested data

--- callbacks/promise/promise.js
+++ callbacks/promise/promise.js
@@ -1,11 +1,14 @@
-const contactURL = "/contact";
+//  "/users"
+// edit next line "/users" directory to fetch other pages i.e "/contact"
+const usersURL = "/users";
 
 const get = url => {
 	return new Promise((resolve, reject) => {
 		const pages = {
 			"/users"   : [
 				{ id: 9, name: "Billy" },
-				{ id: 32, name: "Sophie" }
+				{ id: 32, name: "Sophie" },
+				{ id: 7, name: "Bentley Cooper" }
 			],
 			"/contact" : "Pretend this is the contact page!"
 		};
@@ -15,16 +18,20 @@ const get = url => {
 	});
 };
 
-get(contactURL)
+get(usersURL)
 	.then(response => {
 		console.log(`Status code: ${response.status} OK`);
 		console.log("Resolved. Content accessed.");
-		console.log(
-			`Loading page and revealing contents: ${response.requestedData}`
-		);
+		console.log("Your requested data: ", response.requestedData);
+		// Note: Do not pass an array of objects in a template literal ${}
+		// Example:
+		//  console.log(`data: ${response.requestedData}`);
+		// returns [object Object],[object Object],[object Object]
 	})
 	.catch(response => {
 		console.warn(`There was an error: ${response.status}`);
 	});
 
 // then() runs only if promise is resolved
+// pass in objects in resolve/reject as arguments,
+//  typically with status codes and other properties
