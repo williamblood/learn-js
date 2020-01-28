1a495ecc77abb3a15f56bc081b75b7a22ea197ad
William Blood <william.blood@icloud.com>
2020-01-27 23:57:16 -0800

    Refactored to improve readability

--- JSON/fetch.js
+++ JSON/fetch.js
@@ -1,35 +1,47 @@
-// Note: Use AXIOS instead to deal with the response status
-fetch("https://swapi.co/api/starships/")
-	.then((response) => {
-		// Convert the response body readableStream into JSON data
-		if (!response.ok) throw new Error(response.status);
-		return response.json();
-	})
-	.then((data) => {
-		console.log("Here's everything I got back: ", data);
-		const airships = data.results;
-		console.log("I found ships!", airships); // data is now a javascript object
-		let filmURL = "";
-		airships.forEach((airship) => {
-			if (airship.name.includes("TIE")) {
-				console.log(
-					"Is that a TIE fighter? Check this out!",
-					airship
-				);
-				console.log("Let me find some relevant films..");
-				filmURL = airship.films[0];
-				// Note: cannot return fetch() in if() scope
-			} else console.log(airship.name);
-		});
-		return fetch(filmURL);
-	})
-	.then((response) => {
-		if (!response.ok) throw new Error(response.status);
-		return response.json();
-	})
-	.then((data) => {
-		console.log("Got the first film", data);
-	})
+// Note: AXIOS remains the gold standard, use that instead
+
+const validateAndParseData = (response) => {
+	console.log("Data received. Confirming status...");
+	if (!response.ok) throw new Error(response.status);
+	else console.log(`Status: ${response.status} OK. Parsing data.`);
+	return response.json();
+};
+
+const listAirships = (data) => {
+	console.log("Here's everything I got back: ", data);
+	const airships = data.results;
+	console.log("I found ships!", airships); // data is now a javascript object
+	airships.forEach((airship) => {
+		if (airship.name.includes("TIE")) {
+			console.log(
+				"Is that a TIE fighter? Check this out!",
+				airship
+			);
+		} else {
+			console.log(airship.name);
+		}
+	});
+	return Promise.resolve(data);
+};
+
+const fetchNextPage = (data) => {
+	const nextPage = data.next;
+	console.log("Fetching next page...");
+	return fetch(nextPage);
+};
+
+const fetchStarships = (url = "https://swapi.co/api/starships/") => {
+	console.log(`Fetching data from ${url}`);
+	return fetch(url);
+};
+
+// fetchStarships() is equivalent to fetch("https://swapi.co/api/starships/")
+fetchStarships()
+	.then(validateAndParseData)
+	.then(listAirships)
+	.then(fetchNextPage)
+	.then(validateAndParseData)
+	.then(listAirships)
 	.catch((err) => {
 		console.error(err, "Unable to fetch data. Aborting.");
 	});
