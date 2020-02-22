9635e829fb5440527bc04a03f4853cc576eb9991
williamblood <w.blood@icloud.com>
2020-02-21 21:12:45 -0800

    3 ways to handle errors

--- APIs/async/async.js
+++ APIs/async/async.js
@@ -1,16 +1,55 @@
-// Mimicking async without async
+// Normal error handling function
 function add(x, y) {
-	return new Promise((resolve, reject) => {
-		if (typeof x !== "number" || typeof y !== "number") {
-			reject({ status: "Incorrect type" });
-		} else resolve(x + y);
-	});
+	if (typeof x !== "number" || typeof y !== "number") {
+		throw "ERROR: INCORRECT INPUT";
+	}
+	return x + y;
 }
 
-add(1, 4)
+// Manually with promises
+const divide = (x, y) => {
+	return new Promise((resolve, reject) => {
+		if (typeof x !== "number" || typeof y !== "number") {
+			reject("ERROR: INCORRECT INPUT");
+		}
+		resolve(x / y);
+	});
+};
+
+// Short-hand *BEST PRACTICES*
+async function subtract(x, y) {
+	if (typeof x !== "number" || typeof y !== "number") {
+		throw {
+			status : 200,
+			log    :
+				"\nAsynchronously caught.\nPlease enter a number."
+		};
+	}
+	return x - y;
+}
+
+subtract("x", 4)
 	.then((data) => {
 		console.log(data);
 	})
-	.catch((reject) => {
-		throw new Error(reject.status);
+	.catch(({ status, log }) => {
+		// parameter destructuring
+		throw console.log("Error:", status, log);
 	});
+
+// // Mimicking async without async
+// function add(x, y) {
+// 	return new Promise((resolve, reject) => {
+// 		if (typeof x !== "number" || typeof y !== "number") {
+// 			reject({ status: "Incorrect type" });
+// 		} else resolve(x + y);
+// 	});
+// }
+
+// add(1, 4)
+// 	.then((data) => {
+// 		console.log(data);
+// 	})
+// 	.catch((reject) => {
+// 		throw new Error(reject.status);
+// 	});
