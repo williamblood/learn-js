30515fffa65de0560dde860aa248d98677244695
William Blood <william.blood@icloud.com>
2020-02-23 09:55:24 -0800

    all buttons now reference the Timer class on click

--- Timer/timer.js
+++ Timer/timer.js
@@ -5,18 +5,27 @@ class Timer {
 		this.pause_btn = pause_btn;
 
 		this.start_btn.addEventListener("click", this.start);
-		this.pause_btn.addEventListener("click", this.pause);
+		this.pause_btn.addEventListener(
+			"click",
+			this.pause.bind(this) // binds pause() to any new instance of Timer w/out arrow func
+		);
 	}
+
 	start = () => {
 		this.activateStart();
+		// arrow func binds 'this' to any new Timer using the constructor
 	};
 	activateStart() {
+		// bound inside the constructor of any new Timer instance (using arrow func)
 		console.log(this);
 		console.log("Timer Started.");
 	}
+
 	pause() {
+		// bound inside the constructor of any new Timer instance (using bind())
 		console.log(this);
 		console.log("Paused timer.");
+		// BUG FIXED! bug log -> BUGGED: 'this' references button tag when clicked
 	}
 }
 
@@ -27,3 +36,5 @@ const pauseButton = document.querySelector("#pause");
 const timer = new Timer(inputDuration, startButton, pauseButton);
 timer.start();
 timer.pause();
+
+// Import to Babel for debugging compiled source code and 'this'
