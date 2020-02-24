3baddabf1e4edf8ef3f2783a7f0a8f115854cf28
William Blood <william.blood@icloud.com>
2020-02-24 15:46:24 -0800

    prepped isolation of border styling from logic

--- Timer/timer.js
+++ Timer/timer.js
@@ -1,8 +1,15 @@
 class Timer {
-	constructor(input_dur, start_btn, pause_btn) {
+	constructor(input_dur, start_btn, pause_btn, callbacks) {
 		this.input_dur = input_dur;
 		this.start_btn = start_btn;
 		this.pause_btn = pause_btn;
+
+		// Saves a reference to optional callbacks if provided
+		if (callbacks) {
+			this.onStart = callbacks.onStart;
+			this.onTick = callbacks.onTick;
+			this.onComplete = callbacks.onComplete;
+		}
 
 		this.start_btn.addEventListener("click", this.start);
 		this.pause_btn.addEventListener("click", this.pause);
@@ -17,6 +24,7 @@ class Timer {
 	}
 
 	start = () => {
+		this.onStart();
 		const currentTime = this.timeRemaining; // grabs input val
 		this.timeRemaining = currentTime; // updates timer
 
@@ -29,18 +37,30 @@ class Timer {
 	};
 
 	tick = () => {
-		if (this.timeRemaining < 1) this.pause();
-		else {
+		if (this.timeRemaining < 1) {
+			this.pause();
+			this.onComplete();
+		} else {
 			this.timeRemaining = this.timeRemaining - 1; // ticks down to 0
-			console.log(`${this.timeRemaining}`);
+			this.onTick(this.timeRemaining);
 		}
 	};
 }
 
 const inputDuration = document.querySelector("#duration");
-inputDuration.value = 30;
+inputDuration.value = 30; // default
 
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(inputDuration, startButton, pauseButton);
+const timer = new Timer(inputDuration, startButton, pauseButton, {
+	onTick(value) {
+		console.log(`Time remaining: ${value}`);
+	},
+	onComplete() {
+		console.log("Timer complete.");
+	},
+	onStart() {
+		console.log("Timer started.");
+	}
+});
