5b01d02fc0ef399e15c06f9d8077b9baf587fe04
William Blood <william.blood@icloud.com>
2020-02-24 19:12:42 -0800

    animated svg per tick

--- Timer/index.js
+++ Timer/index.js
@@ -1,10 +1,17 @@
 const inputDuration = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
+const circle = document.querySelector("circle");
 
+const perimeter = 2 * Math.PI * circle.getAttribute("r");
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let currentOffset = 0;
 const timer = new Timer(inputDuration, startButton, pauseButton, {
 	onTick(value) {
 		console.log(`Time remaining: ${value}`);
+		circle.setAttribute("stroke-dashoffset", currentOffset);
+		currentOffset = currentOffset - 50;
 	},
 	onComplete() {
 		console.log("Timer complete.");
